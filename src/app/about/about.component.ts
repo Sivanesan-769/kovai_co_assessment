55391995dd38e5748e61cf2873bedecc7d61c002
Sivanesan Dh <sivanesan.dh@gmail.com>
2024-09-14 20:49:56 +0530

    about implementations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
 
+  product: any;
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParamMap.subscribe((params) => {
+      const data = params.get('data');
+      if (data) {
+        this.product = JSON.parse(data);
+      }
+      console.log('this.product :', this.product);
+    });
+  }
+
+  goBack() {
+    this.router.navigateByUrl('/products');
+  }
 }
--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -3,10 +3,11 @@ import { Component, Input, input } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-
   @Input() product!: any;
-
+  serializedProduct(product: any): any {
+    return JSON.stringify(product);
+  }
 }
