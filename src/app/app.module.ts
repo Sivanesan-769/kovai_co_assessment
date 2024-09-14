e90adb1dcf6648b023540f040dca265884b84772
Sivanesan Dh <sivanesan.dh@gmail.com>
2024-09-14 20:31:19 +0530

    products filter and sorting implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,41 @@ import { provideHttpClient } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+
 import { ProductComponent } from './products/product/product.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, AboutComponent, ProductComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, MatButtonModule, MatCardModule, StoreModule.forRoot({}, {})],
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    AboutComponent,
+    ProductComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    FormsModule,
+    StoreModule.forRoot({}, {}),
+  ],
   providers: [
     provideHttpClient(),
     provideStore(),
-    provideAnimationsAsync()
+    provideAnimationsAsync('animations')
   ],
   bootstrap: [AppComponent],
 })
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,46 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
+  constructor() {}
 
   http = inject(HttpClient);
-  products$ = this.http.get('https://fakestoreapi.com/products') as Observable<any[]>;
+  products$ = this.http.get('https://fakestoreapi.com/products') as Observable<
+    any[]
+  >;
 
+  searchQueryChange(e: any) {
+    this.products$ = this.http.get('https://fakestoreapi.com/products').pipe(
+      map((products: any) => {
+        return products.filter((product: any) =>
+          product.title.toLowerCase().includes(e.toLowerCase())
+        );
+      })
+    );
+  }
+
+  sortQueryChange(e: any) {
+    this.products$ = this.http
+      .get('https://fakestoreapi.com/products')
+      .pipe(map((products: any) => this.sortProducts(products, e)));
+  }
+
+  sortProducts(products: any[], e: any): any[] {
+    const sortOption = e;
+    return [...products].sort((a, b) => {
+      if (sortOption === 'priceAsc') {
+        return a.price - b.price;
+      } else if (sortOption === 'priceDesc') {
+        return b.price - a.price;
+      } else {
+        return a.title.localeCompare(b.title);
+      }
+    });
+  }
 }
